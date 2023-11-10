0e5953056000c1e20e05ab3f42c4e02a964f366d
igorbasko01 <igorbasko@gmail.com>
2023-11-10 07:18:48 +0200

    draw basic ui for an actual exercise (#2)

--- lib/screens/home/number_recognition_page.dart
+++ lib/screens/home/number_recognition_page.dart
@@ -1,5 +1,9 @@
+import 'package:arithmetic_expressions_generator/bloc/number_recognition_bloc.dart';
+import 'package:arithmetic_expressions_generator/bloc/number_recognition_event.dart';
+import 'package:arithmetic_expressions_generator/bloc/number_recognition_state.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NumberRecognitionPage extends StatefulWidget {
   const NumberRecognitionPage({Key? key}) : super(key: key);
@@ -13,30 +17,42 @@ class _NumberRecognitionPageState extends State<NumberRecognitionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Number Recognition')),
-      body: Center(
-        child: Column(
-          children: [
-            const Text('Number Recognition Exercise Here'),
-            const Divider(color: Colors.grey, thickness: 1),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('1'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('2'),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('3'),
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: BlocProvider<NumberRecognitionBloc>(
+        create: (context) => NumberRecognitionBloc(),
+        child: BlocBuilder<NumberRecognitionBloc, NumberRecognitionState>(
+            builder: (blocContext, state) {
+          if (state is InitialNumberRecognitionState) {
+            blocContext
+                .read<NumberRecognitionBloc>()
+                .add(GenerateNewExerciseNumberRecognitionEvent(maxNumber: 10));
+            return _initialView(
+                'Welcome to the Number Recognition!', blocContext);
+          } else if (state is ExerciseNumberRecognitionState) {
+            return _drawExercise(state, blocContext);
+          } else {
+            return Container();
+          }
+        }),
       ),
     );
+  }
+
+  Widget _initialView(String text, BuildContext blocContext) {
+    return Column(children: [
+      Text(text),
+    ]);
+  }
+
+  Widget _drawExercise(
+      ExerciseNumberRecognitionState state, BuildContext blocContext) {
+    return Column(children: [
+      Text('Number of objects is: ${state.numberOfObjects.toString()}'),
+      const Divider(color: Colors.grey, thickness: 1),
+      Row(
+          children: state.possibleAnswers
+              .map((e) =>
+                  ElevatedButton(onPressed: () {}, child: Text(e.toString())))
+              .toList()),
+    ]);
   }
 }
