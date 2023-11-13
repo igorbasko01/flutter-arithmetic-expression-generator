6fcdf2f71912e4e3e9f8aeb98da5b42f601178e9
igorbasko01 <igorbasko@gmail.com>
2023-11-13 22:51:59 +0200

    remove unused import

--- lib/screens/home/number_recognition_page.dart
+++ lib/screens/home/number_recognition_page.dart
@@ -1,7 +1,6 @@
 import 'package:arithmetic_expressions_generator/bloc/number_recognition_bloc.dart';
 import 'package:arithmetic_expressions_generator/bloc/number_recognition_event.dart';
 import 'package:arithmetic_expressions_generator/bloc/number_recognition_state.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
