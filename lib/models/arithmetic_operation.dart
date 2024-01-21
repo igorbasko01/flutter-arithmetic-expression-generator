a025c266b5c33ce47772dd4eabc702188e299355
igorbasko01 <igorbasko@gmail.com>
2024-01-21 18:51:35 +0200

    add bottom sheet settings for arithmetic exercise game, see #16 for details

--- lib/models/arithmetic_operation.dart
+++ lib/models/arithmetic_operation.dart
@@ -2,5 +2,21 @@ enum ArithmeticOperation {
   addition,
   subtraction,
   multiplication,
-  division,
+  division;
+
+  @override
+  String toString() {
+    switch (this) {
+      case ArithmeticOperation.addition:
+        return 'Addition';
+      case ArithmeticOperation.subtraction:
+        return 'Subtraction';
+      case ArithmeticOperation.multiplication:
+        return 'Multiplication';
+      case ArithmeticOperation.division:
+        return 'Division';
+      default:
+        return '';
+    }
+  }
 }
--- lib/screens/home/arithmetic_exercise_game.dart
+++ lib/screens/home/arithmetic_exercise_game.dart
@@ -1,3 +1,4 @@
+import 'package:arithmetic_expressions_generator/models/arithmetic_operation.dart';
 import 'package:flutter/material.dart';
 
 class ArithmeticExerciseGamePage extends StatefulWidget {
@@ -10,14 +11,60 @@ class ArithmeticExerciseGamePage extends StatefulWidget {
 
 class _ArithmeticExerciseGamePageState
     extends State<ArithmeticExerciseGamePage> {
+  int _maxOperandValue = 30;
+  ArithmeticOperation _selectedOperation = ArithmeticOperation.addition;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Arithmetic Exercise Game'),
-        ),
-        body: const Center(
-          child: Text('Arithmetic Exercise Game'),
-        ));
+      appBar: AppBar(
+        title: const Text('Arithmetic Exercise Game'),
+      ),
+      body: const Center(
+        child: Text('Arithmetic Exercise Game'),
+      ),
+      bottomSheet: _settings(),
+    );
+  }
+
+  Widget _settings() {
+    return BottomSheet(
+        onClosing: () {},
+        builder: (BuildContext sheetContext) {
+          return Container(
+              color: Colors.white,
+              height: 200.0,
+              width: double.infinity,
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Max operand value: $_maxOperandValue'),
+                    Slider(
+                        value: _maxOperandValue.toDouble(),
+                        min: 0,
+                        max: 1000,
+                        divisions: 200,
+                        label: '$_maxOperandValue',
+                        onChanged: (double value) {
+                          setState(() {
+                            _maxOperandValue = value.toInt();
+                          });
+                        }),
+                    DropdownButton<ArithmeticOperation>(
+                        value: _selectedOperation,
+                        items: ArithmeticOperation.values
+                            .map((ArithmeticOperation operation) {
+                          return DropdownMenuItem<ArithmeticOperation>(
+                              value: operation,
+                              child: Text(operation.toString()));
+                        }).toList(),
+                        onChanged: (ArithmeticOperation? operation) {
+                          setState(() {
+                            _selectedOperation = operation!;
+                          });
+                        })
+                  ]));
+        });
   }
 }
